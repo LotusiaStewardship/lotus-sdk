34ffa49937664ccd568eb475adaf3ea218999403
maff1989 <matthew@urgero.net>
2025-11-16 16:22:12 -0700

    Add P2P module exports to `index.ts`
    With the new configuration for NPM distribution, it was discovered that not all modules are available at the root level of the package. This commit ensures the P2P modules are exported from the root level.

--- index.ts
+++ index.ts
@@ -6,11 +6,12 @@
 export * from './lib/rpc.js'
 /** Bitcore modules */
 export * as Bitcore from './lib/bitcore/index.js'
+export * as P2P from './lib/p2p/index.js'
 /** RANK modules */
 export * from './lib/rank/api.js'
-export * from './lib/rank/script.js'
-export * from './lib/rank/opcode.js'
-export * from './lib/rank/index.js'
+//export * from './lib/rank/script.js'
+//export * from './lib/rank/opcode.js'
+export * as RANK from './lib/rank/index.js'
 /** Utility modules */
 export * from './utils/constants.js'
 export * from './utils/functions.js'
